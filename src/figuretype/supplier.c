0da59066fd211cef6a2f33f92ed0c86309bb13f0
diretorpc <47946459+diretorpc@users.noreply.github.com>
2024-10-04 12:25:42 -0300

    Update supplier.c

--- src/figuretype/supplier.c
+++ src/figuretype/supplier.c
@@ -245,7 +245,7 @@ void figure_supplier_action(figure *f)
             figure_combat_handle_corpse(f);
             break;
         case FIGURE_ACTION_145_SUPPLIER_GOING_TO_STORAGE:
-            figure_movement_move_ticks(f, 1);
+            figure_movement_move_ticks(f, 4);
             if (f->direction == DIR_FIGURE_AT_DESTINATION) {
                 f->wait_ticks = 0;
                 f->previous_tile_x = f->x;
@@ -282,7 +282,7 @@ void figure_supplier_action(figure *f)
             }
             break;
         case FIGURE_ACTION_146_SUPPLIER_RETURNING:
-            figure_movement_move_ticks(f, 1);
+            figure_movement_move_ticks(f, 4);
             if (f->direction == DIR_FIGURE_AT_DESTINATION || f->direction == DIR_FIGURE_LOST) {
                 if (f->direction == DIR_FIGURE_AT_DESTINATION && f->type == FIGURE_LIGHTHOUSE_SUPPLIER) {
                     building_get(f->building_id)->resources[RESOURCE_TIMBER] += 100;
@@ -378,7 +378,7 @@ void figure_delivery_boy_action(figure *f)
                 leader->type == FIGURE_PRIEST_SUPPLIER || leader->type == FIGURE_PRIEST ||
                 leader->type == FIGURE_BARKEEP_SUPPLIER || leader->type == FIGURE_CARAVANSERAI_SUPPLIER ||
                 leader->type == FIGURE_CARAVANSERAI_COLLECTOR) {
-                figure_movement_follow_ticks(f, 1);
+                figure_movement_follow_ticks(f, 4);
             } else {
                 f->state = FIGURE_STATE_DEAD;
             }
@@ -437,7 +437,7 @@ void figure_fort_supplier_action(figure *f)
             figure_combat_handle_corpse(f);
             break;
         case FIGURE_ACTION_236_SUPPLY_POST_GOING_TO_FORT:
-            figure_movement_move_ticks(f, 1);
+            figure_movement_move_ticks(f, 4);
             if (f->direction == DIR_FIGURE_AT_DESTINATION) {
                 f->action_state = FIGURE_ACTION_237_SUPPLY_POST_RETURNING_FROM_FORT;
                 f->destination_x = f->source_x;
@@ -453,7 +453,7 @@ void figure_fort_supplier_action(figure *f)
             if (f->wait_ticks) {
                 f->wait_ticks--;
             } else {
-                figure_movement_move_ticks(f, 1);
+                figure_movement_move_ticks(f, 4);
                 if (f->direction == DIR_FIGURE_REROUTE) {
                     figure_route_remove(f);
                 } else if (f->direction == DIR_FIGURE_AT_DESTINATION || f->direction == DIR_FIGURE_LOST) {
